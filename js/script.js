c90b0e40c014196a2c34c749385d9824aa60aa33
ahmedbaloch19a <ahmedbadini42062@GMAIL.COM>
2023-05-08 14:42:08 +0500

    My Portfolio Website

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick =  () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+   
+}
+
+
+
+// scroll sections 
+
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll(' header nav a');
+
+window.onscroll = ()  =>{
+    sections.forEach(sec =>{
+        let top =window.scrollY;
+        let offset=sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if ( top>= offset && top < offset +height ){
+            // active navbar links
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+
+            });
+            // active section for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+
+
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+    //sticky HEader 
+
+    let header=document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY >100);
+
+   
+    // Removing Toggle icon & navbar when Click Navbar Links (Scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
